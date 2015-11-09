2b54dc6a6c9293571df3fcf6582fbb6570e44b20
2076831r <gregor_r@btinternet.com>
2015-11-09 17:23:02 +0000

    when you hover over a button it now displays an image of the related card

--- Card.py
+++ Card.py
@@ -1,4 +1,5 @@
 from ttk import Button
+from PIL import Image
 """
     stores all data related to a dice masters card
 """
@@ -8,11 +9,17 @@ class Card:
     rarity = ""
     priority = 0
     button = Button()
+    file_name = ""
 
     def __init__(self, name, priority):
         self.name = name
         self.priority = priority
 
+    def gen_file_name(self):
+        name = self.name.split(": ")
+        name[0] = name[0].replace(" ", "_")
+        name[1] = name[1].replace(" ", "_")
+        self.file_name = "images/" + name[0] + "/" + name[1] + ".jpg"
 
     def display(self):
         print self.name
